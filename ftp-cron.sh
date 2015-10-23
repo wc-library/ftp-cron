2ba5c918e41bd8183eaf052cd15650fa4810996a
bgarcia <brandon.garcia@my.wheaton.edu>
2015-10-23 13:53:26 -0500

    prototype script with config file

---
+++ ftp-cron.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source ftp-cron.conf
+
+options=''
+if $sync_rem; then
+    $options+=' --delete'
+fi
+
+if !$recursive; then
+    $options+=' --no-recursion'
+fi
+
+if $sync_mod && !$sync_new; then
+    $options+=' --only-existing'
+elif $sync_new && !$sync_mod; then
+    $options+=' --only-missing'
+fi
+
+if $verbose; then
+    $options+=' --verbose'
+fi
+
+lftp -f "
+open $remote_host
+user $remote_user $remote_pass
+lcd $remote_dir
+mirror $options $remote_dir $local_dir
+bye
+"
