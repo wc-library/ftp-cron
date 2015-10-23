88fc4b0880bbb86592ca4133a64749042399a0fc
bgarcia <brandon.garcia@my.wheaton.edu>
2015-10-23 14:42:38 -0500

    moved config reading into separate file

--- ftp-cron.sh
+++ ftp-cron.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-source "$1"
+source include/config.sh
+CONFIG_FILE = "$1"
 
-options=''
-if $sync_rem; then
-    $options+=' --delete'
-fi
-
-if !$recursive; then
-    $options+=' --no-recursion'
-fi
-
-if $sync_mod && !$sync_new; then
-    $options+=' --only-existing'
-elif $sync_new && !$sync_mod; then
-    $options+=' --only-missing'
-fi
-
-if $verbose; then
-    $options+=' --verbose'
-fi
+$options = `get_mirror_options $CONFIG_FILE`
 
 lftp -f "
 open $remote_host
